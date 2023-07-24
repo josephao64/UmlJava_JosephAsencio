2f5167e5a936cc57705f42683682ef4f8a3f2ce6
josephao64 <josephasencio64@gmail.com>
2023-07-24 00:22:09 -0600

    se agrego repositorio

---
+++ src/Persona.java
@@ -0,0 +1,23 @@
+//Joseph isaac Asencio Ortiz   Uml - Java
+public abstract class Persona {
+    protected String nombre;
+    protected long descripcion;
+    protected int poder;
+    protected int vida;
+
+    public Persona(String nombre, long descripcion, int poder, int vida) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.poder = poder;
+        this.vida = vida;
+    }
+
+    public abstract void Desplazarse();
+
+    public int MostrarVida() {
+        return vida;
+    }
+
+    public void MostrarPoder() {
+    }
+}
